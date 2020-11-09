8d756141a89da228f9466bc6efda8b2d7b5321fc
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-11-09 16:31:29 -0300

    Change URL names

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="{{ url('/') }}">Navbar</a>
+    <a class="navbar-brand" href="{{ url('/') }}">LemSystems</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -7,13 +7,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{ url('/best') }}">Artist Best Songs</a>
+                <a class="nav-link" href="{{ url('/best') }}">Top Ten Canciones</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ url('/ranking') }}">Ranking</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ url('/excel') }}">Generate Excel</a>
+                <a class="nav-link" href="{{ url('/excel') }}">Generar Excel</a>
             </li>
         </ul>
     </div>
